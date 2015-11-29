06cc06ffe0fda47344aeb3eb5ed20259809bbfa2
Claire <ClaireS29@gmail.com>
2015-11-29 14:20:35 -0800

    Added form to make a trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -6,17 +6,19 @@ class TripsController < ApplicationController
   end
 
   def create
-    name_to_hiker = Hiker.where(["name = ?", trip_params[:trip][:name]]).to_a
+    # name_to_hiker = Hiker.where(["name = ?", trip_params[:trip][:name]]).to_a
     trip = Trip.create(
-    hiker_id: name_to_hiker.id,
+    hiker_id: trip_params[:trip][:hiker_id],
     hike_id: params[:hike_id],
-    notes: trip_params[:trip][:notes]
+    notes: trip_params[:trip][:notes],
+    completed_at: trip_params[:trip][:completed_at]
     )
+    redirect_to hiker_path(trip.hiker_id)
   end
 
   private
 
   def trip_params
-    params.permit(trip: [:name, :notes])
+    params.permit(trip: [:hiker_id, :name, :notes, :completed_at])
   end
 end
