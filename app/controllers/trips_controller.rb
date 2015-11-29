bd57060aa35048956456917cffd5191b29e577f5
Claire <ClaireS29@gmail.com>
2015-11-29 10:07:01 -0800

    Making the form to save a hike via a new trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,22 @@
 class TripsController < ApplicationController
+
+  def new
+    @trip = Trip.new
+    @hikers = Hiker.all
+  end
+
+  def create
+    name_to_hiker = Hiker.where(["name = ?", trip_params[:trip][:name]]).to_a
+    trip = Trip.create(
+    hiker_id: name_to_hiker.id,
+    hike_id: params[:hike_id],
+    notes: trip_params[:trip][:notes]
+    )
+  end
+
+  private
+
+  def trip_params
+    params.permit(trip: [:name, :notes])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root 'hikes#home'
   resources :hikes do
+    resources :trips 
   end
   resources :hikers do
-  end
-  resources :trips do
   end
 end
