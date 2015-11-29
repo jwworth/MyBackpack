07ccfe81665b70135c43a8ff3c48b6622a7f4bd1
Claire <ClaireS29@gmail.com>
2015-11-29 10:44:39 -0800

    Added new, edit, and delete functions for hikers

--- app/controllers/hikers_controller.rb
+++ app/controllers/hikers_controller.rb
@@ -11,6 +11,35 @@ class HikersController < ApplicationController
       @saved_hikes.push(Hike.find(hike.hike_id))
       #puts hike.hike_id
     end
-    @all_dem
+  end
+
+  def new
+    @hiker = Hiker.new
+  end
+
+  def create
+    hiker = Hiker.create(hiker_params[:hiker])
+    redirect_to hiker_path(id: hiker.id )
+  end
+
+  def edit
+    @hiker = Hiker.find(params[:id])
+  end
+
+  def update
+    @hiker = Hiker.find(params[:id])
+    Hiker.update(params[:id], hiker_params[:hiker])
+    redirect_to hiker_path(params[:id])
+  end
+
+  def destroy
+    Hiker.destroy(params[:id])
+    redirect_to hikers_path
+  end
+
+  private
+
+  def hiker_params
+    params.permit(hiker: [:name, :location, :bio])
   end
 end
