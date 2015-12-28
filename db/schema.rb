d7ae34904516fd216b3bc6631dc382f7bcf65feb
emgord <emgord@gmail.com>
2015-12-28 14:11:16 -0800

    added email and password for hikers to seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130070928) do
+ActiveRecord::Schema.define(version: 20151228220640) do
 
   create_table "hikers", force: :cascade do |t|
     t.string   "name"
     t.string   "location"
     t.string   "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "email"
+    t.string   "password_digest"
   end
 
   create_table "hikes", force: :cascade do |t|
