810235539f942e074bda10e45a45b34bc6f65943
emgord <emgord@gmail.com>
2015-12-28 15:22:37 -0800

    added logout and name to home page if logged in, log in and create account if logged out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :current_user
 
   def current_user
     @current_user ||= Hiker.find(session[:hiker_id]) if session[:hiker_id]
